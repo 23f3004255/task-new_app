cfe1131e2f4e5e973e8fb79a8be1ff014dc89dfb
Sriyansh Srivastava <23f3004255@ds.study.iitm.ac.in>
2025-10-16 13:54:29 +0530

    Round 1 commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.buy-btn').forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    alert('Added to cart! This is a demo. Checkout coming soon.');
+  });
+});
